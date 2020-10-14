0e79a58068363b58730cb3b999c1c35df2bd3f54
SophieBenyamin <72621438+SophieBenyamin@users.noreply.github.com>
2020-10-14 16:17:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-$.getJSON('https://api.coingecko.com/api/v3/exchange_rates', function(json) {
-  console.log(json);
-});
+function ajaxRequest(params) {
+    var url = 'https://api.coingecko.com/api/v3/exchange_rates'
+    var datajson = [];
+    $.get(url).then(function (res) {       
+        for (currencycode in res.rates) {
+            currencyobj = res.rates[currencycode];
+            datajson.push({
+                "currencycode": currencycode,
+                "name": currencyobj.name,
+                "type": currencyobj.type,
+                "unit": currencyobj.unit,
+                "value": currencyobj.value
+            });
+        }
+        params.success(datajson)
+    });
+}
